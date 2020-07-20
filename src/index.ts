2419f47aee660f704b6ffc47509cd9a3fb07630a
Thibault Théologien <thibault@nobbas.com>
2020-07-20 14:31:19 +0200

    First draft forms typing

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,45 @@ import * as express from 'express';
 import { SERVER_PORT } from './environment';
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Success' });
-});
+enum InputType {
+  TEXT,
+  INTEGER,
+  FLOAT,
+  BOOLEAN,
+  ENUM,
+}
+
+interface Input {
+  id: number; // generated
+  name: string;
+  type: InputType;
+}
+
+interface Section {
+  id: number; // generated
+  name?: string;
+  subSections: Section[];
+  inputs: Input[];
+}
+
+interface FormTemplate {
+  id: number; // generated
+  name?: string;
+  sections: Section[];
+  inputs: Input[];
+}
+
+// Retrieve all the form-templates
+app.get('/form-templates', (req, res) => {});
+
+// Create a form-template
+app.post('/form-templates', (req, res) => {});
+
+// Update a form-template
+app.put('/form-templates/:id', (req, res) => {});
+
+// Retrieve a form-template
+app.get('/form-templates/:id', (req, res) => {});
 
 app.listen(SERVER_PORT, function () {
   console.log(`Server running on ${SERVER_PORT}!`);
