40e231674e3d322586d0f21a8026c80bbc630b74
Thibault Théologien <thibault@nobbas.com>
2020-07-20 14:19:31 +0200

    Add nodemon config + default express server

---
+++ src/environment.ts
@@ -0,0 +1,3 @@
+// Should use environment variables later on
+
+export const SERVER_PORT = 3000;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('start app');
+import * as express from 'express';
+import { SERVER_PORT } from './environment';
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Success' });
+});
+
+app.listen(SERVER_PORT, function () {
+  console.log(`Server running on ${SERVER_PORT}!`);
+});
